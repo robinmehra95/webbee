7dc68fd3fe9adb3af2389534f7f91a1c39d0a5fd
Robin Mehra <robinmehra95@gmail.com>
2020-05-16 19:04:28 +0530

    router and redux configured

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import configureStore from './store';
+import { BrowserRouter as Router } from "react-router-dom";
+
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+    <Provider store={configureStore()}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
   document.getElementById('root')
 );
